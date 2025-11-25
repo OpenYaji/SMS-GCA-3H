f0d503e54f7a144ec4ecd0031cc4adecb0f78ebb
denmark-111 <maglipon.denmarc.amar@gmail.com>
2025-11-26 04:36:23 +0800

    feat(admin-escort): implement index endpoint for student escort

---
+++ Admin/backend/app/Http/Resources/AuthorizedEscortResource.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AuthorizedEscortResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'EscortID' => $this->EscortID,
+            // 'StudentProfileID' => $this->StudentProfileID,
+            'FullName' => $this->FullName,
+            'RelationshipToStudent' => $this->RelationshipToStudent,
+            'ContactNumber' => $this->ContactNumber,
+            'Address' => $this->Address,
+            'AdditionalNotes' => $this->AdditionalNotes,
+            'EscortStatus' => $this->EscortStatus,
+            'IsActive' => $this->IsActive,
+            'DateAdded' => $this->DateAdded,
+            'ApprovedByUserID' => $this->ApprovedByUserID,
+            'Student' => [
+                'StudentProfileID' => $this->studentProfile->StudentProfileID,
+                'FirstName' => $this->studentProfile->profile->FirstName,
+                'LastName' => $this->studentProfile->profile->LastName,
+                'MiddleName' => $this->studentProfile->profile->MiddleName,
+                'GradeLevel' => $this->studentProfile->enrollments()?->orderByDesc('EnrollmentID')->first()?->section?->gradeLevel?->LevelName,
+                'Section' => $this->studentProfile->enrollments()?->orderByDesc('EnrollmentID')->first()?->section?->SectionName,
+            ],
+        ];
+    }
+}
---
+++ Admin/backend/app/Models/AuthorizedEscort.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AuthorizedEscort extends Model
+{
+
+    protected $table = 'authorized_escort';
+    protected $primaryKey = 'EscortID';
+    public $timestamps = false;
+
+    // protected $fillable = [
+    //     'StudentProfileID',
+    //     'FullName',
+    //     'RelationshipToStudent',
+    //     'ContactNumber',
+    //     'Address',
+    //     'AdditionalNotes',
+    //     'EscortStatus',
+    //     'IsActive',
+    //     'DateAdded',
+    //     'ApprovedByUserID'
+    // ];
+    
+    //Relationship: AuthorizedEscort belongs to a StudentProfile
+    public function studentProfile()
+    {
+        return $this->belongsTo(StudentProfile::class, 'StudentProfileID', 'StudentProfileID');
+    }
+}
